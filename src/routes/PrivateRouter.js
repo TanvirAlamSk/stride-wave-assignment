2f47ecbb6e3c13bf845f26740614613dad10d797
TanvirAlamSk <ta89725@gmail.com>
2024-06-05 12:46:47 +0600

    add loader

--- src/routes/PrivateRouter.js
+++ src/routes/PrivateRouter.js
@@ -1,12 +1,13 @@
 import React, { useContext } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 import { authContext } from '../context/Authprovider';
+import Loader from '../components/shared/Loader';
 
 const PrivateRouter = ({ children }) => {
     const location = useLocation()
     const { user, loader } = useContext(authContext)
     if (loader) {
-        return <p>Loading....</p>
+        return <Loader></Loader>
     }
 
     if (user && user?.uid) {
