eda909c1b0828120fde2d3b7a19d18eee2e28015
TanvirAlamSk <ta89725@gmail.com>
2024-06-19 11:49:52 +0600

    add a loader

--- src/components/home/LatestRecipe/LatestRecipe.js
+++ src/components/home/LatestRecipe/LatestRecipe.js
@@ -16,7 +16,7 @@ const LatestRecipe = () => {
         <div>
             <h1 className='text-5xl font-bold 
             text-center my-10'> Latest Recipes</h1>
-            <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-5 mx-5'>
+            <div className={`${recipes.length > 0 && "grid"} grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-5 mx-5`}>
                 {recipes.length === 0 ?
                     <SecendaryLoader></SecendaryLoader>
                     :
