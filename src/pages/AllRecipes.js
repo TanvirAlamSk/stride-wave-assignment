810a8188c5997e252569e9c12025c3cec9e62f34
TanvirAlamSk <ta89725@gmail.com>
2024-06-19 11:50:45 +0600

    add a loader

--- src/pages/AllRecipes.js
+++ src/pages/AllRecipes.js
@@ -25,7 +25,7 @@ const AllRecipes = () => {
                         <button type='submit' className='border-2 border-red-400 font-bold bg-red-400 rounded-full'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 text-white font-bold opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg></button>
                     </label>
                 </div>
-                <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-5 mx-5'>
+                <div className={`${recipes.length > 0 && "grid"} grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-5 mx-5`}>
                     {allRecipe.length === 0 ?
                         <SecendaryLoader></SecendaryLoader>
                         :
